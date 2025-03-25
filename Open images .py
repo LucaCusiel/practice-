e8ca68fad5717ed165c1b6da8b6116f31b23e145
LucaCusiel <24024@fraser.school.nz>
2025-03-26 11:46:34 +1300

    Update 1
    Added a new python scritpt to open a image if a user says input (will add to future code)
    
    Also added on the main script where it asks the user for a bet and if its higher than 100$ it will say sorry then repeat the input until its valid

---
+++ Open images .py
@@ -0,0 +1,9 @@
+from PIL import Image
+
+answer = input("Do you want to keep gambling: ")
+
+if answer == "yes":
+    print("Horay")
+else:
+    im = Image.open(r"C:\Users\24024\OneDrive - Hamilton's Fraser High School\Documents\GitHub\practice-\KeepGoing.jpg")
+    im.show()
--- Slots Python.py
+++ Slots Python.py
@@ -1,5 +1,17 @@
+import random
+import time
+
 balance = 100
 
-print(f'Balance: {balance}')
+print(f'Balance: ${balance}')
 
-slots = int(input("How much do you wanna bet: "))
+
+while True:
+    bet = int(input("How much do you wanna bet: "))
+    if bet > balance:
+        print("Not Enough Balance, You Poor. Please lower your bet") #Will keep repeating until user gives a bet that is within their balance
+        time. sleep(1)
+    else:
+        break
+
+print("Test")
