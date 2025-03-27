0c324a49a9f33fe5e9774d8c90cea6180ff14f9e
LucaCusiel <24024@fraser.school.nz>
2025-03-27 13:14:44 +1300

    Update
    Prints information about the slots, added symbols as a def and added all the emojis that will be in the slots
    
    also added def for the wheels to tell me what wheel got what

--- Open images .py
+++ Open images .py
@@ -7,3 +7,5 @@ if answer == "yes":
 else:
     im = Image.open(r"C:\Users\24024\OneDrive - Hamilton's Fraser High School\Documents\GitHub\practice-\KeepGoing.jpg")
     im.show()
+    im = Image.open(r"C:\Users\24024\OneDrive - Hamilton's Fraser High School\Documents\GitHub\practice-\KeepGoing.jpg")
+    im.show()
--- Slots Python.py
+++ Slots Python.py
@@ -2,14 +2,27 @@ import random
 import time
 
 balance = 100
+symbols = ["💎","7️⃣","🔔","💗","🍓","🍒","🍋","🍇",]
 
-print(f'Balance: ${balance}')
+firstwheel = None
+secondwheel = None
+thridwheel = None
 
+
+print("Welcome to 🎰Slots🎰")
+time. sleep(0.5)
+print(f'your balance is currently: ${balance}')
+print("To win the jackpot you will need to get 3 diamonds in a whole row")
+print("The jackpot is currently $350")
+print("Every time you bet it will tae half of what you bet and put it into the jackpot pool")
 
 while True:
-    bet = int(input("How much do you wanna bet: "))
-    if bet > balance:
+    betamount = int(input("How much do you wanna bet: "))
+    if betamount > balance:
         print("Not Enough Balance, You Poor. Please lower your bet") #Will keep repeating until user gives a bet that is within their balance
+        time. sleep(1)
+    elif betamount < 1:    
+        print("Please enter a valid bet")
         time. sleep(1)
     else:
         break
