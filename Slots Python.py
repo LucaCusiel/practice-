a484f40daebbac8fde7a6f11bdda20f195e8a5f8
LucaCusiel <24024@fraser.school.nz>
2025-04-30 11:02:56 +1200

    Update
    Added another delayed text typing for the slots only to make it slower showing
    
    Fixed bet input where it wouldn't repeat and give you a error when inputting a wrong number.
    
    Added random code to generate 3 random symbols

--- Slots Python.py
+++ Slots Python.py
@@ -6,6 +6,18 @@ import os
 balance = 100
 symbols = ["💎","7️⃣","🔔","💗","🍓","🍒","🍋","🍇",]
 
+payouts = {
+    "💎": {"2": 1.5, "3": 20},
+    "7️⃣": {"2": 1.5, "3": 5},
+    "🔔": {"2": 1.5, "3": 3},
+    "💗": {"2": 1.5, "3": 2.5},
+    "🍓": {"2": 1.45, "3": 1.75},
+    "🍒": {"2": 1.35, "3": 1.65},
+    "🍋": {"2": 1.2, "3": 1.5},
+    "🍇": {"2": 1.2, "3": 1.5},
+}
+
+
 firstwheel = None
 secondwheel = None
 thridwheel = None
@@ -15,7 +27,15 @@ def delayed_text(s):   #Taken from https://www.youtube.com/watch?v=2h8e0tXHfk0 a
     for c in s:
         sys.stdout.write(c)
         sys.stdout.flush()    
-        time.sleep(0.05)
+        time.sleep(0.0)
+
+
+
+def symbolwait(s):
+    for c in s:
+        sys.stdout.write(c)
+        sys.stdout.flush()    
+        time.sleep(0.5)
 
 
 
@@ -27,16 +47,23 @@ delayed_text("The jackpot is currently $350\n")
 delayed_text("Every time you bet it will take half of what you bet and put it into the jackpot pool\n")
 
 while True:
-    betamount = int(input("How much do you wanna bet: \n"))
+    try:
+        betamount = int(input("Enter your bet: "))
+    except ValueError:
+        print("Please enter a valid bet\n")
     if betamount > balance:
-        delayed_text("Not Enough Balance, You Poor. Please lower your bet\n") #Will keep repeating until user gives a bet that is within their balance
-        time. sleep(1)
+            delayed_text("Not Enough Balance, You Poor. Please lower your bet\n") #Will keep repeating until user gives a bet that is within their balance
+            time. sleep(1)
     elif betamount < 1:    
-        delayed_text("Please enter a valid bet\n")
-        time. sleep(1)
+            delayed_text("Please enter a valid bet\n")
+            time. sleep(1)
     else:
         break
 
 balance = balance - betamount
 delayed_text(f"Rolling Slots With A Bet Of ${betamount}\n")
 delayed_text(f"Balance: ${balance}\n")
+
+random_items = random.sample(symbols, 3)
+time. sleep(0.5)
+symbolwait(random_items)
--- test.py
+++ test.py
@@ -1,5 +1,25 @@
-from PIL import Image
+import random
+import time
+import sys
+import os
 
-while True:
-    im = Image.open(r"C:\Users\24024\OneDrive - Hamilton's Fraser High School\Documents\GitHub\practice-\KeepGoing.jpg")
-    im.show()
+balance = 100
+
+def delayed_text(s):   #Taken from https://www.youtube.com/watch?v=2h8e0tXHfk0 and edited to fit with my code and make it easier to use
+    for c in s:
+        sys.stdout.write(c)
+        sys.stdout.flush()    
+        time.sleep(0.05)
+
+
+
+try:
+    betamount = int(input("Enter your bet: "))
+except ValueError:
+    print("Please enter a valid bet\n")
+if betamount > balance:
+        delayed_text("Not Enough Balance, You Poor. Please lower your bet\n") #Will keep repeating until user gives a bet that is within their balance
+        time. sleep(1)
+elif betamount < 1:    
+        delayed_text("Please enter a valid bet\n")
+        time. sleep(1)
